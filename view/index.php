7e7c48895dd94d6750cb2bd006662b0cd600b588
Simclem <simon.clement98@yahoo.com>
2016-05-05 23:24:09 +0200

    Ajout de la view de l'index.php

---
+++ view/index.php
@@ -0,0 +1,11 @@
+<html lang="fr">
+	<?php //include_once("include/head.php"); ?>
+	<body>
+		<?php// include_once("include/header.php");?>
+		<h1> Présentation </h1>
+			Ceci est le projet tuteuré de Web de Guillaume Jorandon et Simon Clément
+		</br>
+			Le sujet était de faire un site d'échange de carte Monopoly
+			<?php include_once("include/footer.php"); ?>
+	</body>
+</html>
