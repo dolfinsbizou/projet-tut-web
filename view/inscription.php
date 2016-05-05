9d4f452c23f942e0e5ec05a1d99acedfe436d0a2
Simclem <simon.clement98@yahoo.com>
2016-05-05 23:22:49 +0200

    Ajout de la view  du formulaire d'inscription

---
+++ view/inscription.php
@@ -0,0 +1,19 @@
+<html lang = "fr">
+	<?php include_once("include/head.php") ?>
+	<body>
+			<h1>Inscription</h1>
+			<?php //include_once("include/header.php"); ?>
+			<form method="POST" action ="validerInscription.php" >
+				Login : <input type="text" name="login">
+			</br>
+				Mot de passe : <input type="password" name="mdp">
+			</br>
+				Confirmez le mot de passe : <input type="password" name="mdp_conf">
+			</br>
+				e-mail : <input type ="text" name="email">
+			</br>
+				<input type="submit" value="valider">
+			</form>
+			<?php //include_once("include/footer.php"); ?>
+	</body>
+</html>
