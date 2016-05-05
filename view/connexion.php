430ca8b4ba577a4ed647b20ab9c8cf1987c0d44d
Simclem <simon.clement98@yahoo.com>
2016-05-05 20:01:59 +0200

    Ajout d'une view pour la connexion d'un user

---
+++ view/connexion.php
@@ -0,0 +1,17 @@
+<html lang ="fr">
+	<? include_once("include/head.php"); ?>
+	<body>
+			<h1>Connexion</h1>
+			<form method="POST" action ="validerConnexion.php">
+				Pseudo :
+				<input type="text" name="Pseudo">
+			</br>
+				Mot de passe : 
+				<input type="password" name"mdp">
+			</br>
+				<input type="submit" value ="Connexion">
+			</form>
+			<a href = "inscription.php"> Inscrivez-vous </a>
+			<?php //include_once("include/footer.php") ?>
+	</body>
+</html>
