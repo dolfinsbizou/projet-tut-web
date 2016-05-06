82dbee7db92ff35d91e6d9258b1f840cd995ba1b
Simclem <simon.clement98@yahoo.com>
2016-05-06 16:42:25 +0200

    Ajout de la deconnexion

--- view/include/header.php
+++ view/include/header.php
@@ -3,4 +3,18 @@
 	<header>
 		<h1> <a id="logo" href="/"> <img src="/img/Logo_site.png" height="100" alt="logo"/> Monopoly-Exchange </a> </h1>
 	</header>
+	<?php
+	if (isLogged())
+	{
+		?>
+		<?php
+	}
+	else
+	{
+		?>
+		<a href ="/connexion.php">Connexion</a>
+		<?php
+	}
+
+	?>
 </div>
